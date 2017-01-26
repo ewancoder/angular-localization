f7dce0adbb375cd2cc050d06373eb3a21bd438ec
ewancoder <ewancoder@gmail.com>
2017-01-26 11:05:35 +0300

    Revert "Provide translations as configuration parameter"
    This reverts commit d6bb46ae8f864c561bbf0191c11ec8ba31cef308.

--- src/localization.module.ts
+++ src/localization.module.ts
@@ -1,7 +1,6 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { LoggerModule } from 'ewancoder-angular-logger';
-import { Translation } from './translation.model';
-import { provideI18nTranslations } from './i18n.service.provider';
+import { i18nServiceProvider } from './i18n.service.provider';
 import { localeServiceProvider } from './locale.service.provider';
 import { TranslateService } from './translate.service';
 import { TranslatePipe } from './translate.pipe';
@@ -17,11 +16,11 @@ import { TranslatePipe } from './translate.pipe';
     exports: [ TranslatePipe ]
 })
 export class LocalizationModule {
-    static forRoot(translations?: Translation[]): ModuleWithProviders {
+    static forRoot(): ModuleWithProviders {
         return {
             ngModule: LocalizationModule,
             providers: [
-                provideI18nTranslations(translations ? translations : []),
+                i18nServiceProvider,
                 localeServiceProvider,
                 ...LoggerModule.forRoot().providers
             ]
